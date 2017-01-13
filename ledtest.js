6fbb9915a5f67662180fd984f16443384399d23e
sennt18 <sennt18@smca.com>
2017-01-13 11:41:43 -0600

    Update ledtest.js

--- ledtest.js
+++ ledtest.js
@@ -1,5 +1,5 @@
 var Gpio = require('onoff').Gpio,
-led = new Gpio(27, 'out');
+led = new Gpio(18, 'out');
 var iv = setInterval(function(){
 led.writeSync(led.readSync() === 0 ? 1 : 0)
 }, 500);
@@ -9,3 +9,5 @@ clearInterval(iv); // Stop blinking
 led.writeSync(0);  // Turn LED off.
 led.unexport();    // Unexport GPIO and free resources
 }, 5000);
+
+//Gpio17=red, Gpio27=blue, Gpio18=
