53419c87c5b4af7f289e34f84ea3b684650d71b6
sennt18 <sennt18@smca.com>
2017-01-13 12:18:37 -0600

    Update ledtest.js

--- ledtest.js
+++ ledtest.js
@@ -90,16 +90,17 @@ function parseText(str){
 
 
 /*
-var Gpio = require('onoff').Gpio,
-led = new Gpio(18, 'out');
-var iv = setInterval(function(){
-led.writeSync(led.readSync() === 0 ? 1 : 0)
-}, 500);
-// Stop blinking the LED and turn it off after 5 seconds.
-setTimeout(function() {
-clearInterval(iv); // Stop blinking
-led.writeSync(0);  // Turn LED off.
-led.unexport();    // Unexport GPIO and free resources
-}, 5000);
+* var Gpio = require('onoff').Gpio,
+* led = new Gpio(18, 'out');
+* var iv = setInterval(function(){
+* led.writeSync(led.readSync() === 0 ? 1 : 0)
+* }, 500);
+* // Stop blinking the LED and turn it off after 5 seconds.
+* setTimeout(function() {
+* clearInterval(iv); // Stop blinking
+* led.writeSync(0);  // Turn LED off.
+* led.unexport();    // Unexport GPIO and free resources
+* }, 5000);
 
-// Gpio17=red, Gpio27=blue, Gpio18=green
+* // Gpio17=red, Gpio27=blue, Gpio18=green
+*/
