4b863b8c014efe66e6e01974a39f987e2aa2b38e
sennt18 <sennt18@smca.com>
2017-01-13 12:44:46 -0600

    Update simpleledtest.js

--- simpleledtest.js
+++ simpleledtest.js
@@ -3,12 +3,4 @@ led = new Gpio(18, 'out');
 var iv = setInterval(function(){
 led.writeSync(led.readSync() === 0 ? 1 : 1)
 }, 500);
-/*
-// Stop blinking the LED and turn it off after 5 seconds.
-setTimeout(function() {
-clearInterval(iv); // Stop blinking
-led.writeSync(0);  // Turn LED off.
-led.unexport();    // Unexport GPIO and free resources
-}, 5000);
 
-// Gpio17=red, Gpio27=blue, Gpio18=green
