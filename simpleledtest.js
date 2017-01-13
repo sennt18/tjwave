545cea064c1dafb60fd78424aaac4866d825728e
sennt18 <sennt18@smca.com>
2017-01-13 12:36:19 -0600

    Update simpleledtest.js

--- simpleledtest.js
+++ simpleledtest.js
@@ -1,8 +1,8 @@
 var Gpio = require('onoff').Gpio,
 led = new Gpio(18, 'out');
 var iv = setInterval(function(){
-led.writeSync(led.readSync() === 0 ? 1 : .5)
-}, 500);
+led.writeSync(led.readSync() === 0 ? 1)
+};
 
 // Stop blinking the LED and turn it off after 5 seconds.
 setTimeout(function() {
