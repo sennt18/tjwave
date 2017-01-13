b17766799cd7a1faced178c68e6971f52605d77a
sennt18 <sennt18@smca.com>
2017-01-13 09:24:34 -0600

    Update and rename config_sample.js to config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// Please replace the username and password with your bluemix credentials
+
+
+exports.STTPassword = '1Ncdu41AzECG' ;
+exports.STTUsername = '86de684f-89c1-4feb-9b63-c99aa8e95c35' ;
+
+//Credentials for Watson Text to Speech service
+
+exports.TTSPassword = 'LI3tLMdtuZns' ;
+exports.TTSUsername = '6a3d7102-c996-4331-82e5-2757a1a0d5f2' ;
+
+// Credentials for Watson Conversation service
+exports.ConPassword = 'rriZ4oMB4nfE' ;
+exports.ConUsername = 'de0128cd-46d9-4931-bffe-07a429c85c09' ;
+exports.ConWorkspace = '9d507dc0-2074-4a88-a7bd-b19de02df743';
+
+// Credentials for Vision Api
+exports.VisionKey = '5c0918238cbf7a6899c08ed4675e321050540325' ;
+exports.VisionVersion = '2016-05-19' ;
+
+exports.version = 'v1' ;
