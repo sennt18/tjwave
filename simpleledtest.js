c9649200ee3a53f621fa99b99aadffad57f74d86
sennt18 <sennt18@smca.com>
2017-01-13 12:52:15 -0600

    Update simpleledtest.js

--- simpleledtest.js
+++ simpleledtest.js
@@ -4,3 +4,9 @@ var iv = setInterval(function(){
 led.writeSync(led.readSync() === 0 ? 1 : 1)
 }, 500);
 
+// Stop blinking the LED and turn it off after 5 seconds.
+setTimeout(function() {
+    clearInterval(iv); // Stop blinking
+    led.writeSync(0);  // Turn LED off.
+    led.unexport();    // Unexport GPIO and free resources
+}, );
